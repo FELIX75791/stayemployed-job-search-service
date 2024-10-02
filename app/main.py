93a42327fc67d728d0d663383437f378ed3e07c4
Kuai Yu <104323387+C1ar0@users.noreply.github.com>
2024-10-01 22:31:19 -0400

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -6,6 +6,12 @@ from sqlalchemy.future import select  # Import select from sqlalchemy.future
 from app.database import get_db
 from app.models import Job
 from app.schemas import JobCreate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 app = FastAPI()
